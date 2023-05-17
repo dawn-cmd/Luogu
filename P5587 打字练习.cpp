b06313435abac1297604599d987f25638ed440c9
dawn-cmd <hjing7@wisc.edu>
2023-05-17 02:21:30 -0400

    update

--- P5587 打字练习.cpp
+++ P5587 打字练习.cpp
@@ -1,56 +1,58 @@
 #include <bits/stdc++.h>
 #define LL long long
 using namespace std;
+
+// Function to read input text and store it in a vector of strings
+void getText(vector<string>& target) {
+    string tmp;
+    getline(cin, tmp); // Read a line of text
+    while (tmp != "EOF") { // Continue until "EOF" is encountered
+        target.emplace_back(tmp); // Add the line to the vector
+        getline(cin, tmp); // Read the next line
+    }
+}
+
+// Function to remove the last character if it is '<' from a given string
+string removeBack(const string& s) {
+    string ans = "";
+    for (auto c: s) {
+        if (c == '<' && ans != "") { // If the character is '<' and the result string is not empty
+            ans.pop_back(); // Remove the last character
+            continue;
+        }
+        ans.push_back(c); // Add the character to the result string
+    }
+    return ans;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    vector<string> ori;
-    string tmp;
-    getline(cin, tmp);
-    while (tmp != "EOF") {
-        ori.emplace_back(tmp);
-        getline(cin, tmp);
-    }
-    vector<string> txt;
-    getline(cin, tmp);
-    while (tmp != "EOF") {
-        txt.emplace_back(tmp);
-        getline(cin, tmp);
-    }
+
+    vector<string> ori; // Original text
+    getText(ori); // Read the original text
+
+    vector<string> txt; // Text to compare
+    getText(txt); // Read the text to compare
+
     LL id;
     double ans = 0;
     for (LL i = 0, _ = min(ori.size(), txt.size()); i < _; ++i) {
-        tmp = "";
-        for (LL j = 0; j < txt[i].size(); ++j) {
-            if (txt[i][j] == '<') {
-                if (tmp != "") {
-                    tmp.pop_back();
-                }
-                continue;
-            }
-            tmp.push_back(txt[i][j]);
-        }
-        txt[i] = tmp;
-        tmp = "";
-        for (LL j = 0; j < ori[i].size(); ++j) {
-            if (ori[i][j] == '<') {
-                if (tmp != "") {
-                    tmp.pop_back();
-                }
-                continue;
-            }
-            tmp.push_back(ori[i][j]);
-        }
-        ori[i] = tmp;
+        ori[i] = removeBack(ori[i]); // Remove the last character if it is '<' from the original text line
+        txt[i] = removeBack(txt[i]); // Remove the last character if it is '<' from the text to compare line
         id = 0;
         while (id < txt[i].size() && id < ori[i].size()) {
-            if (txt[i][id] == ori[i][id]) {
-                ans += 1;
+            if (txt[i][id] == ori[i][id]) { // If the characters at the same position are the same
+                ans += 1; // Increment the count
             }
-            ++id;
+            ++id; // Move to the next position
         }
     }
+
     LL time;
-    cin >> time;
-    cout << int(ans * 60.0 / double(time) + 0.5) << '\n';
+    cin >> time; // Read the time value
+
+    cout << int(ans * 60.0 / double(time) + 0.5) << '\n'; // Calculate and print the result
+
+    return 0;
 }
