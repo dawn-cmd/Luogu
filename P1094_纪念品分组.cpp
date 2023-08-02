20e2772b5710e2899b31aa51e386ca3713125007
dawn-cmd <hjing7@wisc.edu>
2023-08-02 10:14:06 -0400

    create

---
+++ P1094_纪念品分组.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define LL long long
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    LL w;
+    LL n;
+    cin >> w >> n;
+    vector<LL> p(n);
+    for (int i = 0; i < n; ++i) cin >> p[i];
+    sort(p.begin(), p.end());
+    
+}
