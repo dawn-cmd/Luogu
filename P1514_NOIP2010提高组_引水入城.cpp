9eb500f8790e5f1c5658d68787f8f8d0eae98ad6
dawn-cmd <hjing7@wisc.edu>
2023-08-18 10:07:32 -0400

    create P1514

---
+++ P1514_NOIP2010提高组_引水入城.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define LL long long
+#define mp(a, b) make_pair(a, b)
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+}
+
